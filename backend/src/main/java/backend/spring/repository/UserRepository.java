df6b77ce1cae49116f4487857a827b6f913aa3cd
eunseo <es7837@naver.com>
2025-03-02 13:35:18 +0900

    Intial commit

--- backend/src/main/java/backend/spring/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package backend.spring.repository;
-
-import backend.spring.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
