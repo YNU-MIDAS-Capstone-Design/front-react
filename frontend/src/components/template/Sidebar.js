59e6549068ba160da705cd34cbd28794c2f479ff
YujinB <yujinb0201@gmail.com>
2025-03-17 01:31:08 +0900

    Feat :  Add Navlink to SideBar
    Feat :  Add Navlink to SideBar

--- frontend/src/components/template/Sidebar.js
+++ frontend/src/components/template/Sidebar.js
@@ -1,16 +1,20 @@
 import { useState } from "react"
+import { NavLink } from "react-router-dom";
 import styles from "./Sidebar.module.css"
 
-const Sidebar = ({title, dataarray})=>{
+{/*title : 페이지명, dataarray : 사이드바에 들어갈 페이지명, link : click => 이동할 페이지 주소들*/}
+const Sidebar = ({title, dataarray, link})=>{
     const [click, setclick] = useState(dataarray[0]);
 
-    return(
+    return(        
         <div className={styles.bar}>
             <p className={styles.title}>{title}</p>
-            {dataarray.map((data, key)=>
-                <p key={key} onClick={()=>setclick(data)}
-                className={`${styles.data} ${click===data ? styles.click : ""}`}>{data}</p>
-            )}
+            <div className={styles.Navbox}>
+                {dataarray.map((data, key)=>
+                    <NavLink to={link[key]} onClick={()=>{setclick(data); }} 
+                    className={`${styles.data} ${click===data ? styles.click : ""}`}>{data}</NavLink>
+                )}
+            </div>
         </div>
     )
 }
