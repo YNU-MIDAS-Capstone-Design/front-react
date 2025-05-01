54fdb23e0bf5ad0b0eaec29e6ca1e0f62805d9aa
kyjlab <kkl0110@naver.com>
2025-05-01 12:10:24 +0900

    Feat: add user authentication (Login, register)

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -2,18 +2,73 @@ import styles from "../style/Login.module.css";
 import naverLogo from "../assets/naver.png";
 import kakaoLogo from "../assets/kakao.png";
 import googleLogo from "../assets/google.png";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 const Login = () => {
+    const navigate = useNavigate();
+    const [errorMessage, setErrorMessage] = useState("");
+
+    useEffect(() => {
+        const token = localStorage.getItem("accessToken");
+        if (token) {
+          alert("이미 로그인되어 있습니다.");
+          navigate("/"); // 홈으로 리디렉션
+        }
+    }, []);
+      
+    const [info, setInfo] = useState ({
+        nickname:"",
+        password:"" 
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setInfo((prev) => ({
+          ...prev,
+          [name]: value,
+        }));
+    };
+
+    const login =()=>{
+        axios.post("/api/auth/login",info,{
+            headers: {
+              'Content-Type': 'application/json'
+            }   
+          })
+          .then((res=>{
+            const token = res.data.token;
+            localStorage.setItem("accessToken", token);
+            alert("로그인 되었습니다");
+            navigate("/");
+          }))
+          .catch((err)=>{
+            if (err.response?.status === 400){
+                setErrorMessage("로그인 시간이 만료되었습니다. 다시 로그인해주세요.");
+            }
+            else if (err.response?.status === 401) {
+                setErrorMessage("로그인 정보가 잘못되었습니다. 다시 시도해주세요.");
+              }
+            else{
+                setErrorMessage("죄송합니다. 다시 시도해주세요.")
+            }
+          })
+    }
+
     return(
         <>
         <div className={styles.wrapper}>
             <div style={{fontWeight: "bold", fontSize: "36px"}}>로그인</div>
             <div className={styles.loginBox}>
                 <div className={styles.infoBox}>
-                    <div className={styles.infoItem }>Name</div>
-                    <div className={styles.infoItem }>Password</div>
+                    <input type="text" name="nickname" value={info.nickname} className={styles.infoItem} placeholder="NickName" onChange={handleChange}/>
+                    <input type="password" name="password" value={info.password} className={styles.infoItem}placeholder="Password" onChange={handleChange}/>
                 </div>
-                <button className={styles.loginBtn}>Login</button>
+                <button className={styles.loginBtn} onClick={(e)=>(login(e))}>Login</button>
+                {errorMessage && (
+                    <p style={{ color: "red", marginTop: "10px" }}>{errorMessage}</p>
+                )}
                 <div className={styles.socialLogin}>
                     login with Social Account
                     <div className={styles.logoBox}>
@@ -21,8 +76,6 @@ const Login = () => {
                         <img src = {kakaoLogo} alt="kakao Logo" className={styles.logo}/>
                         <img src = {googleLogo} alt="google Logo" className={styles.logo}/>
                     </div>
-                    Forgot your password
-                    <a href="/login" className={styles.loginLink}>Find Password</a>
                 </div>
             </div>
         </div>
--- frontend/src/pages/Register.js
+++ frontend/src/pages/Register.js
@@ -2,33 +2,93 @@ import styles from "../style/Register.module.css";
 import naverLogo from "../assets/naver.png";
 import kakaoLogo from "../assets/kakao.png";
 import googleLogo from "../assets/google.png";
+import axios from "axios";
+import React , { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const Register = () => {
-    return(
-        <>
-        <div className={styles.wrapper}>
-            <div style={{fontWeight: "bold", fontSize: "36px"}}>회원가입</div>
-            <div className={styles.registerBox}>
-                <div className={styles.infoBox}>
-                    <div className={styles.infoItem }>Name</div>
-                    <div className={styles.infoItem }>Email or PhoneNumber</div>
-                    <div className={styles.infoItem }>Password</div>
-                </div>
-                <button className={styles.create}>Create Account</button>
-                <div className={styles.socialLogin}>
-                    Sign up with Social Account
-                    <div className={styles.logoBox}>
-                        <img src = {naverLogo} alt="Naver Logo" className={styles.logo}/>
-                        <img src = {kakaoLogo} alt="kakao Logo" className={styles.logo}/>
-                        <img src = {googleLogo} alt="google Logo" className={styles.logo}/>
-                    </div>
-                    Already have account?
-                    <a href="/login" className={styles.loginLink}>Log in</a>
-                </div>
-            </div>
-        </div>
-        </>
-    )
+  const navigate = useNavigate();
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const [form, setForm] = useState({
+    nickname: "",
+    email: "",
+    password: "",
+    bio: null,
+    location: null,
+    sns: null,
+    techStacks: [
+      "CSS",
+      "HTML"
+    ]
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const register = () =>{
+    axios
+      .post('/api/auth/signup',form,{
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      })
+      .then((res)=>{
+        alert("회원가입 되었습니다!");
+        navigate("/login");
+      })
+      .catch((err)=>{
+        if(err.response?.status === 400){
+          alert("이메일 형식 오류 입입니다.")
+        }
+        else if (err.response?.status === 409) {
+          const errorCode = err.response.data?.code;
+          if (errorCode === "dn") {
+            setErrorMessage("이미 사용 중인 닉네임입니다.");
+          } else if (errorCode === "de") {
+            setErrorMessage("이미 사용 중인 이메일입니다.");
+          } else {
+            setErrorMessage("중복된 정보가 있습니다.");
+          }
+        } else {
+          setErrorMessage("서버 오류가 발생했습니다.");
+        }
+      })
+  }
+  
+  return(
+      <>
+      <div className={styles.wrapper}>
+          <div style={{fontWeight: "bold", fontSize: "36px"}}>회원가입</div>
+          <div className={styles.registerBox}>
+              <div className={styles.infoBox}>
+                  <input type = "text" name="nickname" value={form.nickname} placeholder="Enter your name" className={styles.infoItem} onChange={handleChange}/>
+                  <input type = "email" name = "email" value={form.email} placeholder = "Email or PhoneNumber" className={styles.infoItem} onChange={handleChange}></input>
+                  <input type="password" name="password" value={form.password} placeholder = "password" className={styles.infoItem} onChange={handleChange}></input>
+              </div>
+              <button className={styles.create} onClick = {(e) => register(e)}>Create Account</button>
+              {errorMessage && (
+                <p style={{ color: "red", marginTop: "10px" }}>{errorMessage}</p>
+              )}
+              <div className={styles.socialLogin}>
+                  Sign up with Social Account
+                  <div className={styles.logoBox}>
+                      <img src = {naverLogo} alt="Naver Logo" className={styles.logo}/>
+                      <img src = {kakaoLogo} alt="kakao Logo" className={styles.logo}/>
+                      <img src = {googleLogo} alt="google Logo" className={styles.logo}/>
+                  </div>
+                  Already have account?
+                  <a href="/login" className={styles.loginLink}>Log in</a>
+              </div>
+          </div>
+      </div>
+      </>
+  )
 }
 
 export default Register
