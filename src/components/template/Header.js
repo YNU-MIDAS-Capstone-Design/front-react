bd0f1f3d696d7ed5f3a68577fb4661c608583bb8
YujinB <yujinb0201@gmail.com>
2025-05-14 17:45:07 +0900

    Fix : Add Header Logo
    Fix : Add Header Logo

--- src/components/template/Header.js
+++ src/components/template/Header.js
@@ -1,12 +1,13 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
+import Devmatch from "../../assets/DevMatch.png"
 
 const Header = () => {
   return (
     <div className={styles.wrapper}>
       <div className={styles.top}>
         <Link to="/" className={styles.logo}>
-          Dev Match
+          <img src={Devmatch} style={{width: "130px", height:"58px;", marginTop : "10px"}}></img>
         </Link>
         <div className={styles.items}>
           <Link to="/project" className={styles.noUnderline}>
