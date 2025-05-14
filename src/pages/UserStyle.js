83d5f254c9deda2e2943846c1175d208800bfe29
YujinB <yujinb0201@gmail.com>
2025-05-14 17:13:16 +0900

    Fix : UserStyle page
    Fix : UserStyle page

--- src/pages/UserStyle.js
+++ src/pages/UserStyle.js
@@ -2,9 +2,54 @@ import {useState} from 'react';
 import styles from '../style/UserStyle.module.css';
 
 function UserStyle(){
-    const stack=['C', 'Java', 'Python', 'Js', 'React', 'HTML/CSS', 'Spring', 'SQL', 'Kotlin' ,'TypeScript', '머신러닝', '데이터 분석', 'Android', 'iOS', 'Unity', 'Swift', 'Vue.js', 'Linux', 'JQuery'];
+    const stack =[
+
+        //백엔드
+        "Nodejs",
+        "Java",
+        "Spring",
+        "Express",
+        "Django",
+        "Flask",
+        "NestJS",
+        "MySQL",
+        "MongoDB",
+        "Docker",
+        "AWS",
+        
+        //프론트
+        "JavaScript",
+        "HTML",
+        "CSS",
+        "React",
+        "Vue",
+        
+        //디자이너
+        "Figma",
+        "Illustrator",
+        "Adobe_XD",
+        
+        //모바일 앱
+        "Android",
+        "Kotlin",
+        "Swift",
+        "Flutter",
+        
+        //인공지능
+        "Python",
+        "AI",
+        "BigData",
+        
+        //게임+기타
+        "Unity",
+        "UnrealEngine",
+        "C_SHARP",  //C#
+        "C_PLUS",   //C++
+        "IoT",
+        "security"
+    ];
     const career=['학생', '전공자', '비전공자', '회사원', '프리랜서', '기타'];
-    const locate=['서울특별시','인천광역시', '대구광역시', '부산광역시', '대전광역시', '광주광역시', '울산광역시',' 세종특별시', '경기도', '강원도', '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도', '제주특별자치도'];
+    const locate=['경기도', '강원도', '충청북도', '충청남도', '전라북도', '전라남도', '경상북도', '경상남도'];
     const[stage, setstage] = useState(0);
     const[click, setclick] = useState(0);
     const[multiselect, setmultiselect]=useState([]);
@@ -18,13 +63,13 @@ function UserStyle(){
 
     function stageDot(){
         const result=[];
-        for(let i=1; i<5; i++) result.push(<div className={`${styles.StageDot} ${stage>=i ? styles.Buttonclick : ""}`}>{i}</div>)
+        for(let i=1; i<5; i++) result.push(<div key={i} className={`${styles.StageDot} ${stage>=i ? styles.Buttonclick : ""}`}>{i}</div>)
         return result; }
 
     function MbtiButton(mbti){
         const result=[];
         for(let i=0; i < mbti.length; i++)
-            result.push(<div className={`${styles.Button} ${styles.MbtiButton} ${select[mbti]===mbti[i] ? styles.Buttonclick : ""}`} 
+            result.push(<div key={i} className={`${styles.Button} ${styles.MbtiButton} ${select[mbti]===mbti[i] ? styles.Buttonclick : ""}`} 
             onClick={()=>{setselect((prev)=>{return select[mbti]===mbti[i] ? {...prev, [mbti]:-1} : {...prev, [mbti]: mbti[i]}});}}>
             {mbti[i]}</div>)
         return result;}
@@ -38,12 +83,12 @@ function UserStyle(){
         <p className={styles.title}>당신의 스타일을 알려주세요</p>
         <div className={styles.MiddleBox}>
             {stack.map((item, key)=>
-                <div onClick={()=>{setmultiselect((prev)=>[...prev,key])}} 
+                <div key={key} onClick={()=>{setmultiselect((prev)=>[...prev,key])}} 
                 className={`${styles.Button} ${isactive(key)%2 ? styles.Buttonclick : ""}`}>{item}</div>)}
         </div>
         <div className={styles.MiddleBox}>
             {career.map((item, key)=>
-            <div onClick={()=>{setselect((prev)=>{return select.care===key ? {...prev, care:-1} : {...prev, care:key}});}} 
+            <div key={key} onClick={()=>{setselect((prev)=>{return select.care===key ? {...prev, care:-1} : {...prev, care:key}});}} 
             className={`${styles.Button} ${select.care===key ? styles.Buttonclick : ""}`}>{item}</div>)}
         </div>
         <div className={styles.MiddleBox}>
@@ -56,13 +101,13 @@ function UserStyle(){
             <p className={styles.Dropdown}>위치</p>
             <div>
             <button onClick={()=>{setclick(!click);}} className={`${styles.Dropdown} ${select.loca === -1 ? "": styles.Buttonclick}`}>
-                <div></div>
+                <div style={{paddingLeft:"10px"}}></div>
                 {select.loca > -1 ?  `${locate[select.loca]}` : `시/도`}
-                <i class={click==0 ? "fas fa-caret-down xl" : "fas fa-caret-up xl"}></i>
+                <i className={click==0 ? "fas fa-caret-down xl" : "fas fa-caret-up xl"} style={{paddingRight:"10px"}}></i>
             </button>
             <ul className={`${click==0 ? styles.hidden : `${styles.Dropdown} ${styles.visible}`}`}>
             {locate.map((item, key)=> 
-            <li className={styles.DropList} onClick={()=>{setclick(!click); setselect((prev)=>{return {...prev,loca:key}})}}>{item}</li> )}
+            <li key={key} className={styles.DropList} onClick={()=>{setclick(!click); setselect((prev)=>{return {...prev,loca:key}})}}>{item}</li> )}
             </ul>
             </div>
         </div>
