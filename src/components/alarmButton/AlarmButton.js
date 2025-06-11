73a43e8a88d8b4db675269ae8aa951b29c8aba7f
YujinB <yujinb0201@gmail.com>
2025-06-11 23:37:05 +0900

    add : modal animation
    add : modal animation

--- src/components/alarmButton/AlarmButton.js
+++ src/components/alarmButton/AlarmButton.js
@@ -1,17 +1,32 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./AlarmButton.module.css";
 import NotificationModal from "./NotificationModal";
 import ChatBubbleOutlineRoundedIcon from '@mui/icons-material/ChatBubbleOutlineRounded';
 import NotificationsNoneOutlinedIcon from '@mui/icons-material/NotificationsNoneOutlined';
+
 // import ChatModal from "./ChatModal";
 
 const AlarmButton = ({ isOpen, onClose }) => {
   const [activeTab, setActiveTab] = useState("notification");
+  const [shouldRender, setShouldRender] = useState(false); //modal 제거
+  const [animateClass, setAnimateClass] = useState(""); //fadein,out 적용
 
-  if (!isOpen) return null;
+  useEffect(() => {
+    if (isOpen) {
+      setShouldRender(true);
+      setAnimateClass(styles.modalopen);
+    } else {
+      setAnimateClass(styles.modalclose);
+      setTimeout(() => {
+        setShouldRender(false);
+      }, 300); // fadein, fadeout 적용
+    }
+  }, [isOpen]);
+
+  if (!shouldRender) return null; //fadeout 후 DOM 제거
 
   return (
-    <div className={styles.bottomPanel}>
+    <div className={`${styles.bottomPanel} ${animateClass}`}>
       <div className={styles.tabContent}>
         {activeTab === "notification" && <NotificationModal />}
         {/* {activeTab === "chat" && <ChatBubbleOutlineRoundedIcon />} */}
